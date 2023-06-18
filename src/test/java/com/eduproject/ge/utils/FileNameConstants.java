d7cb7f494ac5c863cf3b5cb06902743cf0146e7c
evapomper <eugenia.altunina@gmail.com>
2023-06-18 17:04:38 +0400

    added var json schema

--- src/test/java/com/eduproject/ge/utils/FileNameConstants.java
+++ src/test/java/com/eduproject/ge/utils/FileNameConstants.java
@@ -4,5 +4,6 @@ public class FileNameConstants {
 
     public static final String BASE_PATH = "./src/test/resources/";
     public static final String POST_API_REQUEST_BODY = BASE_PATH+"postrequestbody.txt";
+    public static final String JSON_SCHEMA = BASE_PATH+"expectedjsonschema.txt";
 }
 
