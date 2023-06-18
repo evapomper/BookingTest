dfede1b1aec1af1b29d000ce18e9251d71f1a1e5
evapomper <eugenia.altunina@gmail.com>
2023-06-18 18:14:19 +0400

    added constants

--- src/test/java/com/eduproject/ge/utils/FileNameConstants.java
+++ src/test/java/com/eduproject/ge/utils/FileNameConstants.java
@@ -5,5 +5,9 @@ public class FileNameConstants {
     public static final String BASE_PATH = "./src/test/resources/";
     public static final String POST_API_REQUEST_BODY = BASE_PATH+"postrequestbody.txt";
     public static final String JSON_SCHEMA = BASE_PATH+"expectedjsonschema.txt";
+    public static final String GET_TOKEN_BODY = BASE_PATH+"gettokendata.txt";
+    public static final String PUT_API_REQUEST_BODY = BASE_PATH+"putrequestbody.txt";
+    public static final String PATCH_API_REQUEST_BODY = BASE_PATH+"patchrequestbody.txt";
+
 }
 
