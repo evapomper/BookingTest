f88417ec0be6be2bd2ed9dea4e2a987f07d8b889
evapomper <eugenia.altunina@gmail.com>
2023-06-18 17:18:47 +0400

    added validation json schema

--- src/test/java/com/eduproject/ge/tests/PostApiRequestUsingPojos.java
+++ src/test/java/com/eduproject/ge/tests/PostApiRequestUsingPojos.java
@@ -2,13 +2,19 @@ package com.eduproject.ge.tests;
 
 import com.eduproject.ge.pojos.Booking;
 import com.eduproject.ge.pojos.BookingDates;
+import com.eduproject.ge.utils.FileNameConstants;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import org.apache.commons.io.FileUtils;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class PostApiRequestUsingPojos {
 
@@ -16,6 +22,8 @@ public class PostApiRequestUsingPojos {
     public void postApiRequest() throws JsonProcessingException {
 
         try {
+            String jsonSchema = FileUtils.readFileToString(new File(FileNameConstants.JSON_SCHEMA), "UTF-8");
+
             BookingDates bookingdates = new BookingDates("2022-01-01", "2023-01-01");
             Booking booking = new Booking("pojos name", "pojos lname", "something", true, 800, bookingdates);
 
@@ -53,9 +61,10 @@ public class PostApiRequestUsingPojos {
                                 .get("/{bookingID}", bookingId)
                             .then()
                                 .assertThat()
-                                .statusCode(200);
+                                .statusCode(200)
+                                .body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
 
-        } catch (JsonProcessingException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
